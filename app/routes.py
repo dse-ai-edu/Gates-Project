fda79dd86e313aada61d4d42bd39c43f9778a2fe
Korbin Yang <86302887+KorbinY@users.noreply.github.com>
2026-06-01 13:59:31 -0700

    Kqyang filter 0513 (#21)
    * add no-answer-leakage to feedback; add issue-to-avoid to feedback postprocess
    
    * debug example component
    
    * debug example component

--- app/routes.py
+++ app/routes.py
@@ -105,7 +105,6 @@ def page_1():
 
 @bp_main.route("/page_2")
 def step2():
-
     return render_template(
         "page_2.html",
         pattern_items=PATTERN_ITEMS,
@@ -123,3 +122,19 @@ def page_final_example():
     return render_template(
         "page_final_example.html"
     )
+    
+
+@bp_main.route('/api/configuration/showexample', methods=['POST'])
+def show_keyword_feedback_example():
+    """Save final configuration and mark system as complete"""
+    data = request.get_json()
+    keywordLongText = data.get('keywordLongText')
+    try:
+        example_body = return_keyword_combination_example(keywords_str=keywordLongText, default_keywords=DEFAULT_KEYWORDS, example_path=EXAMPLE_PATH)
+        example_body['success'] = True
+        response = example_body
+    except:
+        traceback.print_exc()
+        response = {'success': False}
+    return jsonify(response)
+
